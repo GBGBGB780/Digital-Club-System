4b231b76200ebf929c657bc2906b1a8710d7d210
吕杰 <3098320650@qq.com>
2024-05-16 21:12:15 +0800

    后端：学生个人社团错误修复

--- club_student/src/main/java/com/chinahitech/shop/bean/IndividualGroup.java
+++ club_student/src/main/java/com/chinahitech/shop/bean/IndividualGroup.java
@@ -11,7 +11,7 @@ import lombok.*;
 public class IndividualGroup {
     //    private String id;
     @JsonProperty("username")
-    private int IndividualId;
+    private int individualId;
     private String groupId;
     private String studentId;
     private String position;
