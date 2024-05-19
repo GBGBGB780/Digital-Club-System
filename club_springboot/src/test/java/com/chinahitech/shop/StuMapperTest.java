7f32882a81a32508685c8d0d713da56654ec48c2
陈天蔚2131160 <1485415837@qq.com>
2024-05-19 15:54:59 +0800

    StuMapper测试集的构建

--- club_springboot/src/test/java/com/chinahitech/shop/StuMapperTest.java
+++ club_springboot/src/test/java/com/chinahitech/shop/StuMapperTest.java
@@ -33,4 +33,64 @@ public class StuMapperTest {
         System.out.println(user1);
         System.out.println("测试通过");
     }
+
+    @Test
+    public void updateStudentTest() {
+        User user = new User();
+        user.setUserId("21311377");
+        user.setPassword("666666");
+        Date date = new Date();
+
+        int a = stuMapper.updatePassword(user.getUserId(),user.getPassword(), date);
+        System.out.println(a);
+
+        User user1 = stuMapper.getByNum(user.getUserId());
+        System.out.println(user1);
+        System.out.println("测试通过");
+    }
+
+    @Test
+    public void updatePhoneTest() {
+        User user = new User();
+        user.setUserId("21311377");
+        user.setPhone("13333333333");
+        Date date = new Date();
+
+        int a = stuMapper.updatePhone(user.getUserId(),user.getPhone(), date);
+        System.out.println(a);
+
+        User user1 = stuMapper.getByNum(user.getUserId());
+        System.out.println(user1);
+        System.out.println("测试通过");
+    }
+
+    @Test
+    public void updateDescriptionTest() {
+        User user = new User();
+        user.setUserId("21311377");
+        user.setDescription("hhh");
+        Date date = new Date();
+
+        int a = stuMapper.updateDescription(user.getUserId(),user.getDescription(), date);
+        System.out.println(a);
+
+        User user1 = stuMapper.getByNum(user.getUserId());
+        System.out.println(user1);
+        System.out.println("测试通过");
+    }
+
+    @Test
+    public void updateNicknameTest() {
+        User user = new User();
+        user.setUserId("21311377");
+        user.setNickname("GB");
+        Date date = new Date();
+
+        int a = stuMapper.updateNickname(user.getUserId(),user.getNickname(), date);
+        System.out.println(a);
+
+        User user1 = stuMapper.getByNum(user.getUserId());
+        System.out.println(user1);
+        System.out.println("测试通过");
+    }
 }
