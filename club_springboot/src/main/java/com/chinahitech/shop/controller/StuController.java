387966b647a4fe87064af875fe200872b8545cca
吕杰 <3098320650@qq.com>
2024-06-24 20:15:14 +0800

    后端部分功能修复

--- club_springboot/src/main/java/com/chinahitech/shop/controller/StuController.java
+++ club_springboot/src/main/java/com/chinahitech/shop/controller/StuController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("/student")
+    @RequestMapping("/student")
 @CrossOrigin
 public class StuController {
     @Autowired
