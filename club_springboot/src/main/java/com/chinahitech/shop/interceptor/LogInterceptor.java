c61a7d83216bda641f34d296a6cdab7dcb12c8f2
吕杰 <3098320650@qq.com>
2024-06-26 19:40:48 +0800

    后端日志功能开发

---
+++ club_springboot/src/main/java/com/chinahitech/shop/interceptor/LogInterceptor.java
@@ -0,0 +1,24 @@
+package com.chinahitech.shop.interceptor;
+
+import cn.hutool.core.util.RandomUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 日志拦截器
+ */
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        return true;
+    }
+
+}
+
